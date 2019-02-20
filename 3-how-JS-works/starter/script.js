d07fa3f1787847d61d523621f1883bc5d8e1fd19
HappyMell <mellisamadrid@hotmail.com>
2019-02-20 17:00:28 +0000

    coding challenge completed

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,18 +1,35 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+/*
+//functions
+calcAge(1990);
+function calcAge(year) {
+    console.log(2016 - year);
+}
+
+//retirement(1990);
+
+var retirement = function (year) {
+    console.log(65 - (2016 - year));
+}
+
+//variables
+var age = 23;
+console.log(age);
 
 
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
-
-
-
-
-
-
+*/
 
 
 
@@ -36,8 +53,8 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
 
+*/
 
 
 // Example to show the differece between execution stack and scope chain
@@ -69,9 +86,37 @@ function third() {
 
 
 
+//console.log(this);
+
+//calcAge(1990);
+
+//function calcAge(year) {
+//    console.log(2016 - year);
+//    console.log(this);
+//}
+
+var mellisa = {
+    name: "Mellisa",
+    yearOfBirth: 1990,
+
+    calcAge: function () {
+        console.log(this);
+        console.log(2019 - this.yearOfBirth);
+
+        /*function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+}
+mellisa.calcAge();
 
 
+var ben = {
+    name: "Ben",
+    yearOfBirth: 1989
+};
 
-
-
-
+ben.calcAge = mellisa.calcAge;
+ben.calcAge();
